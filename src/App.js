3bbd5024e9305406a78b89ec231a95b4349c0af0
Stefano Albano <ALBSTE@btsbioengineering.com>
2022-05-23 12:17:50 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import CssBaseline from '@mui/material/CssBaseline';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Slide from '@mui/material/Slide';
+import Content from './components/content';
+
+
+function HideOnScroll(props) {
+    const { children, window } = props;
+    const trigger = useScrollTrigger({
+        target: window ? window() : undefined,
+    });
+
+    return (
+        <Slide appear={false} direction="down" in={!trigger}>
+            {children}
+        </Slide>
+    );
+}
+
+HideOnScroll.propTypes = {
+    children: PropTypes.element.isRequired,
+    window: PropTypes.func,
+};
+
+export default function App() {
+   
+    return (
+        <React.Fragment>
+            <CssBaseline />
+            <HideOnScroll>
+                <AppBar>
+                    <Toolbar>
+                        <Typography variant="h6" component="div"> Albums </Typography>
+                    </Toolbar>
+                </AppBar>
+            </HideOnScroll>
+            <Toolbar />
+            <Container>                
+                <Box sx={{ my: 2 }}>
+                    <Content/>
+                </Box>
+            </Container>
+        </React.Fragment>
+    );
+}

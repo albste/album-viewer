4504ba8278e2cf635c5f794df75732392e9495f4
Stefano Albano <ALBSTE@btsbioengineering.com>
2022-05-26 12:48:46 +0200

    BugFixed

---
+++ client/build/precache-manifest.387bc401a9ef63c4dcfe5324c95aff16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "51ff02f62a33bc6e6315",
+    "url": "/album-viewer/static/css/main.afef5b9e.chunk.css"
+  },
+  {
+    "revision": "51ff02f62a33bc6e6315",
+    "url": "/album-viewer/static/js/main.51ff02f6.chunk.js"
+  },
+  {
+    "revision": "c41c1c1a55ea0027eb7b",
+    "url": "/album-viewer/static/js/1.c41c1c1a.chunk.js"
+  },
+  {
+    "revision": "4674cd77088521ef95f9",
+    "url": "/album-viewer/static/js/2.4674cd77.chunk.js"
+  },
+  {
+    "revision": "4565a965a4fbf1762c13",
+    "url": "/album-viewer/static/js/runtime~main.4565a965.js"
+  },
+  {
+    "revision": "49acd58625f76743f57ef7048df57e5b",
+    "url": "/album-viewer/index.html"
+  }
+];
